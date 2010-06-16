9da4f1ef02b76fdeb93a14dd28889fc800923738
Prabin <prabindeka@gmail.com>
2010-06-16 17:17:15 +1000

    Removed some of the callbacks as they are not required

--- Classes/fitnessViewController.m
+++ Classes/fitnessViewController.m
@@ -37,8 +37,8 @@
         self.savedSearchTerm = nil;
     }
     
-    [self.tableView reloadData];
-	self.tableView.scrollEnabled = YES;
+//    [self.tableView reloadData];
+//	self.tableView.scrollEnabled = YES;
 }
 
 - (void)viewDidUnload
@@ -137,53 +137,53 @@
 
 #pragma mark -
 #pragma mark Content Filtering
-
-- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
-{
-    /*
-     Update the filtered array based on the search text and scope.
-     */
-    
-    [self.filteredListContent removeAllObjects]; // First clear the filtered array.
-    
-    /*
-     Search the main list for products whose type matches the scope (if selected) and whose name matches searchText; add items that match to the filtered array.
-     */
-    for (Product *product in listContent)
-    {
-        if ([scope isEqualToString:@"All"] || [product.type isEqualToString:scope])
-        {
-            NSComparisonResult result = [product.name compare:searchText options:(NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch) range:NSMakeRange(0, [searchText length])];
-            if (result == NSOrderedSame)
-            {
-                [self.filteredListContent addObject:product];
-            }
-        }
-    }
-}
-
+//
+//- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+//{
+//    /*
+//     Update the filtered array based on the search text and scope.
+//     */
+//    
+//    [self.filteredListContent removeAllObjects]; // First clear the filtered array.
+//    
+//    /*
+//     Search the main list for products whose type matches the scope (if selected) and whose name matches searchText; add items that match to the filtered array.
+//     */
+//    for (Product *product in listContent)
+//    {
+//        if ([scope isEqualToString:@"All"] || [product.type isEqualToString:scope])
+//        {
+//            NSComparisonResult result = [product.name compare:searchText options:(NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch) range:NSMakeRange(0, [searchText length])];
+//            if (result == NSOrderedSame)
+//            {
+//                [self.filteredListContent addObject:product];
+//            }
+//        }
+//    }
+//}
+//
 
 #pragma mark -
 #pragma mark UISearchDisplayController Delegate Methods
 
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
-{
-    [self filterContentForSearchText:searchString scope:
-	 [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
+//- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+//{
+//    [self filterContentForSearchText:searchString scope:
+//	 [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
     
     // Return YES to cause the search result table view to be reloaded.
-    return YES;
-}
+//    return NO;
+//}
 
 
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
-{
-    [self filterContentForSearchText:[self.searchDisplayController.searchBar text] scope:
-	 [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
-    
-    // Return YES to cause the search result table view to be reloaded.
-    return YES;
-}
+//- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+//{
+//    [self filterContentForSearchText:[self.searchDisplayController.searchBar text] scope:
+//	 [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
+//    
+//    // Return YES to cause the search result table view to be reloaded.
+//    return YES;
+//}
 
 
 @end
